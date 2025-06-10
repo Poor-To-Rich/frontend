f31a715d66bf070b73fc67b074ce70fee8858ef0
woic-ej <woic0102@naver.com>
2025-06-10 17:15:38 +0900

    Feat: 로그아웃 시 쿼리키 삭제 및 TapBar talk 탭 주석처리

--- src/hooks/apis/auth/useLogout.ts
+++ src/hooks/apis/auth/useLogout.ts
@@ -1,17 +1,19 @@
 import { logout } from '@/api/services/authService';
 import { tokenManager } from '@/utils/tokenManager';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 
 const useLogout = () => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: logout,
     onSuccess: data => {
       toast.success(data.message);
       tokenManager.clearToken();
+      queryClient.clear();
       navigate('/login');
     },
     onError: error => toast.error(error.message),
