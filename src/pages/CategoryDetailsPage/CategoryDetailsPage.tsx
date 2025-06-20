2b5aae8e91618b6e59dbba020f57a46418f96c7d
woic-ej <woic0102@naver.com>
2025-06-20 15:39:02 +0900

    Feat: 차트 관련 페이지 에러 처리

--- src/pages/CategoryDetailsPage/CategoryDetailsPage.tsx
+++ src/pages/CategoryDetailsPage/CategoryDetailsPage.tsx
@@ -2,27 +2,34 @@ import DefaultHeader from '@/components/header/DefaultHeader';
 import { useLocation } from 'react-router-dom';
 import CategoryOverviewChart from '@/pages/CategoryDetailsPage/components/CategoryOverviewChart';
 import CategoryLogListSection from '@/pages/CategoryDetailsPage/components/Log/CategoryLogListSection';
+import PageErrorBoundary from '@/components/error/PageErrorBoundary';
+import FetchErrorBoundary from '@/components/error/FetchErrorBoundary';
 
 const CategoryDetailsPage = () => {
   const location = useLocation();
   const { categoryName, transactionType, reportType, categoryId, date, isSavings } = location.state || {};
+  const isWeekly = reportType === '월별';
 
   return (
     <div className="w-full min-h-screen flex flex-col">
       <DefaultHeader label={categoryName} hasBackButton />
-      <CategoryOverviewChart
-        reportType={reportType}
-        transactionType={transactionType}
-        categoryId={categoryId}
-        date={date}
-        isSavings={isSavings}
-      />
-      <CategoryLogListSection
-        transactionType={transactionType}
-        categoryId={categoryId}
-        date={date}
-        isSavings={isSavings}
-      />
+      <PageErrorBoundary>
+        <FetchErrorBoundary key={`${date}-${categoryId}-${isWeekly}`}>
+          <CategoryOverviewChart
+            transactionType={transactionType}
+            categoryId={categoryId}
+            date={date}
+            isWeekly={isWeekly}
+            isSavings={isSavings}
+          />
+          <CategoryLogListSection
+            transactionType={transactionType}
+            categoryId={categoryId}
+            date={date}
+            isSavings={isSavings}
+          />
+        </FetchErrorBoundary>
+      </PageErrorBoundary>
     </div>
   );
 };
