4d816025d23c6818d90f4c7ce08de3957b87f5b3
woic-ej <woic0102@naver.com>
2025-06-25 20:29:34 +0900

    Feat: 404 페이지 추가

--- src/components/error/fallbacks/NotFoundFallback.tsx
+++ src/components/error/fallbacks/NotFoundFallback.tsx
@@ -2,7 +2,7 @@ const NotFoundFallback = () => {
   return (
     <div className="w-full flex flex-col justify-center items-center gap-5">
       <h2 className="text-lg font-bold">404 NotFound</h2>
-      <p className="text-defaultGrey">데이터를 찾을 수 없습니다.</p>
+      <p className="text-defaultGrey">요청하신 리소스를 찾을 수 없습니다.</p>
     </div>
   );
 };
