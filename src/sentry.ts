1ce55c32c9ffdc2963c42c8b8030df1102ffb60f
woic-ej <woic0102@naver.com>
2025-06-26 21:28:15 +0900

    Chore: sentry 배포 환경에서만 동작하도록 수정

--- src/sentry.ts
+++ src/sentry.ts
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/react';
 
 Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
+  dsn: import.meta.env.DEV ? undefined : import.meta.env.VITE_SENTRY_DSN,
   integrations: [Sentry.browserTracingIntegration()],
   tracesSampleRate: 0.3,
   environment: import.meta.env.MODE,
