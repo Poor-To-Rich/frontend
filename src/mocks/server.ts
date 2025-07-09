a1468c2ba3565a70db50a4b55c05745e6d8271e5
woic-ej <woic0102@naver.com>
2025-07-09 22:40:08 +0900

    Feat: 테스트 간 상태 공유 오류로 테스트 파일 분리

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -4,11 +4,13 @@ import { emailHandlers } from '@/mocks/handler/emailHandlers';
 import { tokenHandlers } from '@/mocks/handler/tokenHandlers';
 import { transactionHandlers } from '@/mocks/handler/transactionHandlers';
 import { categoryHandlers } from '@/mocks/handler/categoryHandlers';
+import { totalHandlers } from '@/mocks/handler/totalHandlers';
 
 export const server = setupServer(
   ...tokenHandlers,
   ...authHandlers,
   ...emailHandlers,
   ...transactionHandlers,
+  ...totalHandlers,
   ...categoryHandlers,
 );
