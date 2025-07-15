04adfff25c71db69e0f6f9fecc770fcf1b3764c6
woic-ej <woic0102@naver.com>
2025-07-15 15:54:35 +0900

    fix: Authorization 헤더 Bearer 키워드 추가

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -18,7 +18,7 @@ apiClient.interceptors.request.use(config => {
   const token = tokenManager.getToken();
 
   if (token) {
-    config.headers.Authorization = token;
+    config.headers.Authorization = `Bearer ${token}`;
   }
 
   if (config.data instanceof FormData) {
