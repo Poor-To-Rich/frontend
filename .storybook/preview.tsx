c8711dac1a505429a659f111d71c7a4a4a45e6fc
woic-ej <woic0102@naver.com>
2025-07-21 17:51:53 +0900

    Chore: storybook preview 세팅 및 msw handler 수정

---
+++ .storybook/preview.tsx
@@ -0,0 +1,31 @@
+import type { Preview } from '@storybook/react';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import '../src/index.css';
+
+const queryClient = new QueryClient();
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    Story => {
+      return (
+        <MemoryRouter>
+          <QueryClientProvider client={queryClient}>
+            <Story />
+          </QueryClientProvider>
+        </MemoryRouter>
+      );
+    },
+  ],
+};
+
+export default preview;
