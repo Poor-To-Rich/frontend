1bb5432e8f7f609ce3cf19c596bd6c7e42b3ecdc
woic-ej <woic0102@naver.com>
2025-08-16 19:59:48 +0900

    Feat: 유저 프로필 이미지 상세 보기 모달 생성

---
+++ src/pages/ChatroomPage/components/modal/UserProfileImageModal.tsx
@@ -0,0 +1,19 @@
+import XIconButton from '@/components/button/icon/XIconButton';
+
+interface Props {
+  closeUserProfileImageModal: () => void;
+  profileImage: string;
+}
+
+const UserProfileImageModal = ({ closeUserProfileImageModal, profileImage }: Props) => {
+  return (
+    <div className="fixed inset-0 z-10 flex items-stretch justify-center">
+      <div className="w-[500px] h-full flex justify-center items-center bg-black relative ">
+        <XIconButton color="white" size={28} className="absolute top-3 left-3" onClick={closeUserProfileImageModal} />
+        <img src={profileImage} alt="유저 프로필 이미지" className="w-full max-h-[80%] object-contain bg-white" />
+      </div>
+    </div>
+  );
+};
+
+export default UserProfileImageModal;
