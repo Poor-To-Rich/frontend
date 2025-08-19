d6210bc546e490b4d724028f039a72615587c7f8
woic-ej <woic0102@naver.com>
2025-08-19 10:53:41 +0900

    Feat: 전체 인원 조회에서 유저 프로필 조회 모달 추가

--- src/components/profile/UserProfile.tsx
+++ src/components/profile/UserProfile.tsx
@@ -1,13 +1,19 @@
 import ProfilePhoto from '@/components/photo/ProfilePhoto';
 import CrownIcon from '@/components/icon/CrownIcon';
+import useModal from '@/hooks/useModal';
+import UserProfileModal from '@/pages/ChatroomPage/components/modal/UserProfileModal';
 import { UserProfileType } from '@/types/profileType';
 
-interface Props extends UserProfileType {
+interface Props {
+  userProfile: UserProfileType;
+  chatroomId: string;
   createAt?: string;
   hideRanking?: boolean;
 }
 
-const UserProfile = ({ profileImage, nickname, userId, isHost, rankingType, createAt, hideRanking }: Props) => {
+const UserProfile = ({ userProfile, chatroomId, createAt, hideRanking }: Props) => {
+  const { profileImage, rankingType, nickname, isHost } = userProfile;
+  const { isOpen, openModal, closeModal } = useModal();
   return (
     <div className={'flex gap-5 items-center'}>
       <ProfilePhoto
@@ -15,7 +21,7 @@ const UserProfile = ({ profileImage, nickname, userId, isHost, rankingType, crea
         rankingType={rankingType}
         hideRanking={hideRanking}
         className="w-20"
-        onClick={() => console.log(userId)}
+        onClick={openModal}
       />
       <div className="flex flex-col items-start gap-1.5">
         <div className="flex items-center gap-2 ">
@@ -24,6 +30,7 @@ const UserProfile = ({ profileImage, nickname, userId, isHost, rankingType, crea
         </div>
         {createAt && <p className="text-sm text-defaultGrey">{createAt}</p>}
       </div>
+      {isOpen && <UserProfileModal chatroomId={chatroomId} userProfile={userProfile} closeModal={closeModal} />}
     </div>
   );
 };
