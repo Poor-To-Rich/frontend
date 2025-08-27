e09f6abe3e26865b6130ea066dcd947b36ae5b9e
woic-ej <woic0102@naver.com>
2025-08-27 13:23:50 +0900

    Chore: Sentry 로깅 정보 추가

--- src/utils/error/CustomError.ts
+++ src/utils/error/CustomError.ts
@@ -1,12 +1,33 @@
-class CustomError<T = undefined> extends Error {
+import type { AxiosError, AxiosResponse } from 'axios';
+
+class CustomError<T = any, D = any> extends Error implements AxiosError<T, D> {
+  config!: AxiosError<T, D>['config'];
+  code?: string;
+  request?: any;
+  response?: AxiosResponse<T, D>;
+  isAxiosError = true as const;
+
   statusCode: number;
   data?: T;
 
   constructor(message: string, statusCode: number, data?: T) {
     super(message);
     this.name = this.constructor.name;
-    this.data = data;
     this.statusCode = statusCode;
+    this.data = data;
+
+    Object.setPrototypeOf(this, CustomError.prototype);
+  }
+
+  toJSON() {
+    return {
+      message: this.message,
+      name: this.name,
+      statusCode: this.statusCode,
+      code: this.code,
+      config: this.config,
+      response: this.response,
+    };
   }
 }
 
