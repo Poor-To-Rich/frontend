036da4f24d528ddbe73be18dafd0bb109d915e98
woic-ej <woic0102@naver.com>
2025-09-01 18:12:00 +0900

    Feat: 검색어 유지 기능 추가

--- src/hooks/apis/chat/useSearchChatrooms.tsx
+++ src/hooks/apis/chat/useSearchChatrooms.tsx
@@ -2,7 +2,6 @@ import { searchChatrooms } from '@/api/services/chatService';
 import { useQuery } from '@tanstack/react-query';
 
 const useSearchChatrooms = (keyword: string) => {
-  console.log(keyword);
   return useQuery({
     queryKey: ['searchChatrooms', keyword],
     queryFn: () => searchChatrooms(keyword),
