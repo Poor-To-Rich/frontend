501ffcfb1870883830fe7076b7f097df01086684
woic-ej <woic0102@naver.com>
2025-09-04 17:34:58 +0900

    Hotfix: 참여중인 채팅방 웹 소켓 실시간 처리 로직 수정

--- src/hooks/chat/useHandleUpdateJoinedChatroom.ts
+++ src/hooks/chat/useHandleUpdateJoinedChatroom.ts
@@ -1,37 +1,52 @@
 import { joinedChatroomsQueryKey } from '@/constants/queryKeys';
 import { JoinedChatroomsRes, JoinedChatroomType } from '@/types/chatTypes';
+import { ChatroomUpdatedType } from '@/types/messageType';
 import { InfiniteData, useQueryClient } from '@tanstack/react-query';
 
 const useHandleUpdateJoinedChatroom = () => {
   const queryClient = useQueryClient();
 
-  return (updatedChatroom: JoinedChatroomType) => {
+  return (type: ChatroomUpdatedType, updatedChatroom: JoinedChatroomType) => {
     queryClient.setQueryData(joinedChatroomsQueryKey, (oldData: InfiniteData<JoinedChatroomsRes> | undefined) => {
       if (!oldData) return oldData;
 
-      if (!updatedChatroom.unreadMessageCount || updatedChatroom.unreadMessageCount <= 0) {
+      if (type === 'CHATROOM_INFO_UPDATED') {
         return {
           ...oldData,
-          pages: oldData.pages.map(page => ({
-            ...page,
-            chatrooms: page.chatrooms.map(chatroom =>
+          pages: oldData.pages.map(page => {
+            // 1) 같은 방 찾아서 업데이트
+            const updated = page.chatrooms.map(chatroom =>
               chatroom.chatroomId === updatedChatroom.chatroomId ? updatedChatroom : chatroom,
-            ),
-          })),
+            );
+
+            return {
+              ...page,
+              chatrooms: updated,
+            };
+          }),
         };
       }
 
-      if (updatedChatroom.unreadMessageCount > 0) {
+      if (type === 'CHATROOM_MESSAGE_UPDATED') {
         return {
           ...oldData,
           pages: oldData.pages.map((page, idx) => {
+            // 1) 기존에 같은 방 제거
             const filtered = page.chatrooms.filter(chatroom => chatroom.chatroomId !== updatedChatroom.chatroomId);
 
             if (idx === 0) {
-              return {
+              // 2) 첫 번째 페이지에 updatedChatroom 넣기
+              const updatedPage = {
                 ...page,
                 chatrooms: [updatedChatroom, ...filtered],
               };
+
+              // 3) lastMessageTime 기준 정렬 (내림차순: 최신 먼저)
+              updatedPage.chatrooms.sort(
+                (a, b) => new Date(b.lastMessageTime).getTime() - new Date(a.lastMessageTime).getTime(),
+              );
+
+              return updatedPage;
             }
 
             return {
