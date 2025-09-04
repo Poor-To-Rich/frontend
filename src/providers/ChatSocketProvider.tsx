c2ecc32e64aa8b06acebdbc71c765fde904309bf
woic-ej <woic0102@naver.com>
2025-09-04 17:37:44 +0900

    Feat: 참여중인 채팅방 웹 소켓 구독 ChatSocketProvider에서 관리 하도록 수정

--- src/providers/ChatSocketProvider.tsx
+++ src/providers/ChatSocketProvider.tsx
@@ -1,15 +1,47 @@
 import { ReactNode, useEffect } from 'react';
-import { ensureActive, stompClient } from '@/api/stomp';
+import { ensureActive, stompClient, addOnConnect } from '@/api/stomp';
+import { StompSubscription } from '@stomp/stompjs';
+import useGetUserDetails from '@/hooks/apis/auth/useGetUserDetails';
+import useHandleUpdateJoinedChatroom from '@/hooks/chat/useHandleUpdateJoinedChatroom';
+import { useQueryClient } from '@tanstack/react-query';
+import { joinedChatroomsQueryKey } from '@/constants/queryKeys';
 
 export default function ChatSocketProvider({ children }: { children: ReactNode }) {
+  const { data: userDetail } = useGetUserDetails();
+  const queryClient = useQueryClient();
+  const handleUpdatedJoinedChatroom = useHandleUpdateJoinedChatroom();
+
   useEffect(() => {
     ensureActive();
     return () => {
-      setTimeout(() => {
+      if (stompClient.active) {
         stompClient.deactivate();
-      }, 50);
+      }
+      queryClient.removeQueries({ queryKey: joinedChatroomsQueryKey });
     };
   }, []);
+
+  useEffect(() => {
+    if (!userDetail?.userId) return;
+
+    let sub: StompSubscription | undefined;
+
+    const subscribe = () => {
+      sub = stompClient.subscribe(`/sub/chat/summary/${userDetail.userId}`, res => {
+        const response = JSON.parse(res.body);
+        console.log(response);
+        handleUpdatedJoinedChatroom(response.type, response.payload);
+      });
+    };
+
+    if (stompClient.connected) subscribe();
+    const off = addOnConnect(subscribe);
+
+    return () => {
+      off();
+      sub?.unsubscribe();
+    };
+  }, [userDetail?.userId, handleUpdatedJoinedChatroom]);
 
   return <>{children}</>;
 }
