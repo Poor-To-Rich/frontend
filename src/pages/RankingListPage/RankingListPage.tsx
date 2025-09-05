20a63f907a12cb31fdca2a8ff4e9a9131366ee5e
woic-ej <woic0102@naver.com>
2025-09-05 17:49:09 +0900

    Feat: 랭킹 로딩 및 에러 핸들링

--- src/pages/RankingListPage/RankingListPage.tsx
+++ src/pages/RankingListPage/RankingListPage.tsx
@@ -3,26 +3,15 @@ import LeftArrowButton from '@/components/button/icon/LeftArrowButton';
 import DefaultHeader from '@/components/header/DefaultHeader';
 import RankingInfoModal from '@/components/modal/chat/RankingInfoModal';
 import ModalDimmed from '@/components/modal/ModalDimmed';
-import useGetAllRakingListInfiniteQuery from '@/hooks/apis/ranking/useGetAllRakingListInfiniteQuery';
-import useInfiniteScroll from '@/hooks/useInfiniteScroll';
 import useModal from '@/hooks/useModal';
-import { useRef } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import RankingItem from '@/pages/RankingListPage/components/RankingItem';
+import { useNavigate } from 'react-router-dom';
+import RankingList from '@/pages/RankingListPage/components/RankingList';
+import PageErrorBoundary from '@/components/error/PageErrorBoundary';
+import FetchErrorBoundary from '@/components/error/FetchErrorBoundary';
 
 const RankingListPage = () => {
   const navigate = useNavigate();
-  const { chatroomId } = useParams();
   const { isOpen, openModal, closeModal } = useModal();
-  const { data, hasNextPage, isFetchingNextPage, fetchNextPage } = useGetAllRakingListInfiniteQuery(chatroomId!);
-
-  const observerRef = useRef<HTMLLIElement | null>(null);
-  const allRankingList = data?.pages?.flatMap(page => page.rankings ?? []) || [];
-  const isEmpty = allRankingList?.length === 0;
-
-  console.log(isEmpty);
-
-  useInfiniteScroll({ observerRef, hasNextPage, isFetchingNextPage, fetchNextPage });
 
   return (
     <div className="w-full min-h-screen flex flex-col relative">
@@ -31,21 +20,16 @@ const RankingListPage = () => {
         label="랭킹"
         rightButton={<HelpTooltipButton onClick={openModal} />}
       />
-      {isEmpty ? (
-        <div className="w-full flex-grow flex items-center justify-center text-defaultGrey">랭킹이 없습니다</div>
-      ) : (
-        <ul>
-          {allRankingList.map((ranking, index) => (
-            <RankingItem key={ranking.rankingId} {...ranking} hasUnderLine={index < allRankingList.length} />
-          ))}
-          {!isEmpty && hasNextPage && <li ref={observerRef} className="h-4" />}
-        </ul>
-      )}
-      {isOpen && (
-        <ModalDimmed onClose={closeModal}>
-          <RankingInfoModal closeModal={closeModal} />
-        </ModalDimmed>
-      )}
+      <PageErrorBoundary>
+        <FetchErrorBoundary>
+          <RankingList />
+          {isOpen && (
+            <ModalDimmed onClose={closeModal}>
+              <RankingInfoModal closeModal={closeModal} />
+            </ModalDimmed>
+          )}
+        </FetchErrorBoundary>
+      </PageErrorBoundary>
     </div>
   );
 };
---
+++ src/pages/RankingListPage/components/RankingList.tsx
@@ -0,0 +1,46 @@
+import LoadingSpinner from '@/components/loading/LoadingSpinner';
+import useGetAllRakingListInfiniteQuery from '@/hooks/apis/ranking/useGetAllRakingListInfiniteQuery';
+import useInfiniteScroll from '@/hooks/useInfiniteScroll';
+import RankingItem from '@/pages/RankingListPage/components/RankingItem';
+import { useRef } from 'react';
+import { useParams } from 'react-router-dom';
+
+const RankingList = () => {
+  const { chatroomId } = useParams();
+  const { data, hasNextPage, isFetchingNextPage, fetchNextPage, isPending } = useGetAllRakingListInfiniteQuery(
+    chatroomId!,
+  );
+
+  const observerRef = useRef<HTMLLIElement | null>(null);
+  const allRankingList = data?.pages?.flatMap(page => page.rankings ?? []) || [];
+  const isEmpty = allRankingList?.length === 0;
+
+  useInfiniteScroll({ observerRef, hasNextPage, isFetchingNextPage, fetchNextPage });
+
+  if (isPending) {
+    return (
+      <div className="flex flex-grow items-center justify-center">
+        <LoadingSpinner size={30} />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      {isEmpty ? (
+        <div className="w-full flex-grow flex items-center justify-center text-defaultGrey">랭킹이 없습니다</div>
+      ) : (
+        <ul>
+          {allRankingList.map((ranking, index) => (
+            <li key={ranking.rankingId}>
+              <RankingItem {...ranking} hasUnderLine={index < allRankingList.length} />
+            </li>
+          ))}
+          {!isEmpty && hasNextPage && <li ref={observerRef} className="h-4" />}
+        </ul>
+      )}
+    </>
+  );
+};
+
+export default RankingList;
